c51c292236d21f249d5f1d97008d16a4432f52f6
NileshPathak <pathaknilesh1998@gmail.com>
2022-11-07 10:28:46 +0530

    printing_variables

--- bin/main.dart
+++ bin/main.dart
@@ -7,4 +7,15 @@ void main() {
   var Name_lastname = "Nilesh pathak";
   var address1 = "SavtaNager";
   var $money = 100;
-} 
+
+  //Invlid Variable
+  // not start number in variable name integer
+  //var 1person = "Kunal";
+  //var for  = "Male"
+
+  // printing a varialble%
+  print(name);
+  print(Name_lastname);
+  print(address1);
+  print($money);
+}
