3b74b3376adb79bc8cf567b7da95ab64b887d928
NileshPathak <pathaknilesh1998@gmail.com>
2022-11-07 10:13:07 +0530

    first commit

---
+++ bin/main.dart
@@ -0,0 +1,10 @@
+void main() {
+  // print("hello world");
+
+  //variable and Rules 
+  //Valid Variable
+  var name = "nilesh";
+  var Name_lastname = "Nilesh pathak";
+  var address1 = "SavtaNager";
+  var $money = 100;
+}
