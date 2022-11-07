5c69f64d18ad336a92a4343998431eccfca3ae6e
NileshPathak <pathaknilesh1998@gmail.com>
2022-11-07 11:31:43 +0530

    Operators

--- bin/main.dart
+++ bin/main.dart
@@ -23,30 +23,78 @@ void main() {
 
   //Built-in-data-Type
 
-  int card_item = 2;
-  print(card_item);
-  //calculation 2 number
-  int a = 3;
-  int b = 5;
-  int c = a + b;
-  print(c);
+  // int card_item = 2;
+  // print(card_item);
+  // //calculation 2 number
+  // int a = 3;
+  // int b = 5;
+  // int c = a + b;
+  // print(c);
 
-  //double data type
-  double price = 10.4;
-  print(price);
+  // //double data type
+  // double price = 10.4;
+  // print(price);
 
-  //string data type
-  String City = "Nashik";
-  print(City);
+  // //string data type
+  // String City = "Nashik";
+  // print(City);
 
-  //bool datatype
-  bool is_complited = true;
-  print(is_complited);
-  bool not_complited = false;
-  print(not_complited);
+  // //bool datatype
+  // bool is_complited = true;
+  // print(is_complited);
+  // bool not_complited = false;
+  // print(not_complited);
 
-  var quantity = 100;
-  print(quantity);
-  print(quantity.runtimeType); 
-  
+  // var quantity = 100;
+  // print(quantity);
+  // print(quantity.runtimeType);
+
+  // var Cost = "name";
+  // print(Cost);
+  // print(quantity.runtimeType);
+
+  // var doublevalue = 56.0;
+  // print(doublevalue);
+  // print(doublevalue.runtimeType);
+
+  // var male = true;
+
+  // print(male);
+  // print(male.runtimeType);
+
+  //#Operators
+  //##Arithmetic Operators
+// just like - ,+ ,==
+
+  // int a = 10;
+  // int b = 20;
+  // int c = 30;
+  // int d = 3;
+  // print(a + b);
+  // print(a - c);
+  // print(a * d);
+  // print(a / d);
+  // print(a ~/ c);
+  // print(a % c);
+//Prefix and Postfix Increment and Decrement opreators
+
+  // var i = 3;
+  // print(i);
+  // print(++i);
+  // print(i);
+
+  // var m = 5;
+  // print(m);
+  // print(m++);
+  // print(m);
+
+  // var t = 10;
+  // print(t);
+  // print(--t);
+  // print(t);
+
+  var t = 20;
+  print(t);
+  print(t--);
+  print(t);
 }
