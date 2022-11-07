00a0f12bb9198ce318882182040737f9d9f3cb23
NileshPathak <pathaknilesh1998@gmail.com>
2022-11-07 10:58:24 +0530

    variable

--- bin/main.dart
+++ bin/main.dart
@@ -1,12 +1,14 @@
+import 'dart:ffi';
+
 void main() {
   // print("hello world");
 
   //variable and Rules
   //Valid Variable
-  var name = "nilesh";
-  var Name_lastname = "Nilesh pathak";
-  var address1 = "SavtaNager";
-  var $money = 100;
+  // var name = "nilesh";
+  // var Name_lastname = "Nilesh pathak";
+  // var address1 = "SavtaNager";
+  // var $money = 100;
 
   //Invlid Variable
   // not start number in variable name integer
@@ -14,8 +16,37 @@ void main() {
   //var for  = "Male"
 
   // printing a varialble%
-  print(name);
-  print(Name_lastname);
-  print(address1);
-  print($money);
+  // print(name);
+  // print(Name_lastname);
+  // print(address1);
+  // print($money);
+
+  //Built-in-data-Type
+
+  int card_item = 2;
+  print(card_item);
+  //calculation 2 number
+  int a = 3;
+  int b = 5;
+  int c = a + b;
+  print(c);
+
+  //double data type
+  double price = 10.4;
+  print(price);
+
+  //string data type
+  String City = "Nashik";
+  print(City);
+
+  //bool datatype
+  bool is_complited = true;
+  print(is_complited);
+  bool not_complited = false;
+  print(not_complited);
+
+  var quantity = 100;
+  print(quantity);
+  print(quantity.runtimeType); 
+  
 }
